16799a47635efa8dc0f7921f50741b7b17e96278
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-09-06 16:34:13 +0700

    resolve depends

--- TnCameraMaster/src/main/TnCameraMasterApp.swift
+++ TnCameraMaster/src/main/TnCameraMasterApp.swift
@@ -12,28 +12,10 @@ import TnCameraBase
 
 @main
 struct TnCameraMasterApp: App {
-//    private let motionOrientation: DeviceMotionOrientationListener = DeviceMotionOrientationListener.shared
-//    @StateObject var cameraModel: TnCameraViewModel = .init() /*cameraModelInstance*/
-    
-    var bodyMain: some Scene {
+    var body: some Scene {
         WindowGroup {
             CameraAppView()
                 .background(.black)
-//                .environmentObject(cameraModel)
-//                .onAppear {
-//                    motionOrientation.start()
-//                    
-//                    UITableViewCell.appearance().backgroundColor = UIColor(Color.red.opacity(0.5))
-//                    UITableView.appearance().separatorStyle = .none
-//                    UITableView.appearance().backgroundColor = UIColor(Color.red.opacity(0.5))
-//                }
-//                .onDisappear {
-//                    motionOrientation.stop()
-//                }
         }
-    }
-    
-    var body: some Scene {
-        bodyMain
     }
 }
