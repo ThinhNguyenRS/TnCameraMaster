c7cc93def266c844e994eda77c7ddf0d2c67232e
Thinh Nguyen <thinhnguyen.risinglights@gmail.com>
2024-10-16 19:49:06 +0700

    transcoding

--- TnCameraMaster/src/TnCameraMasterApp.swift
+++ TnCameraMaster/src/TnCameraMasterApp.swift
@@ -11,26 +11,14 @@ import TnIosBase
 import TnCameraBase
 
 @main
-struct TnCameraMasterApp: App {
-//    init() {
-//        print("test async ...")
-//
-//        let t = TestAsync()
-//        t.callCompletion(x: 100)
-//        t.callCompletion(x: 0)
-//        
-//        t.callAsync(x: 100)
-//        t.callAsync(x: 0)
-//        
-//        print("test async !")
-//    }
-        
+struct TnCameraMasterApp: App {    
     var body: some Scene {
         WindowGroup {
             TnCameraAppView(
                 master: true,
                 bleInfo: TnCameraProxyServiceInfo.getBle(),
-                transportingInfo: TnCameraProxyServiceInfo.getTransporting(EOM: nil, MTU: nil, encoder: TnBinaryEncoder(), decoder: TnBinaryDecoder())
+                transportingInfo: TnCameraProxyServiceInfo.getTransporting(EOM: nil, MTU: nil, encoder: TnBinaryEncoder(), decoder: TnBinaryDecoder()),
+                delegate: nil
             )
         }
     }
